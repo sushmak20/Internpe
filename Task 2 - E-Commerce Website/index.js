0c2ee7145ba5aebd1d6428c3ff60aedced703cf7
sushmak20 <109718094+sushmak20@users.noreply.github.com>
2023-07-08 21:50:29 +0530

    Add files via upload

---
+++ Task 2 - E-Commerce Website/index.js
@@ -0,0 +1,20 @@
+const hamburer = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
+
+if (hamburer) {
+  hamburer.addEventListener("click", () => {
+    navList.classList.toggle("open");
+  });
+}
+const popup = document.querySelector(".popup");
+const closePopup = document.querySelector(".popup-close");
+if (popup) {
+  closePopup.addEventListener("click", () => {
+    popup.classList.add("hide-popup");
+  });
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      popup.classList.remove("hide-popup");
+    }, 1000);
+  });
+}
